f04cde55eea340d31e4f7d7a79f2e7864daa8147
Drew Nase <dnasecs@gmail.com>
2018-02-25 11:03:07 -0500

    add checkpoints and seed nodes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -107,13 +107,15 @@ public:
         base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
         vSeeds.push_back(CDNSSeedData("0","54.174.152.228"));
+        vSeeds.push_back(CDNSSeedData("1","nodeuseast.friendshipcoin.com"));
+        vSeeds.push_back(CDNSSeedData("2","nodeuswest.friendshipcoin.com"));
 
         convertSeeds(vFixedSeeds, pnSeed, ARRAYLEN(pnSeed), nDefaultPort);
 
         nPoolMaxTransactions = 3;
         //strSporkKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
         //strMasternodePaymentsPubKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
-        strDarksendPoolDummyAddress = "c25gtaAQYBUNjYrxzZqNdyADSmwPPpWoLC";
+        strDarksendPoolDummyAddress = "f25gtaAQYBUNjYrxzZqNdyADSmwPPpWoLC";
         nLastPOWBlock = 20000;
         nPOSStartBlock = 1;
         nMasternodePaymentStartBlock = 50;
