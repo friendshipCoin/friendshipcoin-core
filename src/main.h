26bcad8225e64d7645bfe363c147f98bfc699bec
Drew Nase <dnasecs@gmail.com>
2018-02-24 23:24:15 -0500

    fix block version

--- src/main.h
+++ src/main.h
@@ -627,7 +627,7 @@ class CBlock
 {
 public:
     // header
-    static const int CURRENT_VERSION = 7;
+    static const int CURRENT_VERSION = 1;
     int nVersion;
     uint256 hashPrevBlock;
     uint256 hashMerkleRoot;
